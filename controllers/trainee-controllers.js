16c48f2549942dd7b89e87b68eb350e81bfba8f1
Ramasamy3488 <91617803+Ramasamy3488@users.noreply.github.com>
2025-08-21 19:09:21 +0530

    Update trainee-controllers.js

--- controllers/trainee-controllers.js
+++ controllers/trainee-controllers.js
@@ -16,7 +16,7 @@ function readAllTrainees(req, res) {
 // Read specific Trainee by Name/Email
 function readATrainee(req, res) {
     try {
-         let { search } = req.body;
+         let { search } = req.query;
 
         TraineesModel.find({ $or: [{ "name": name }, { "email": email }] })
             .then(trainees => {
@@ -99,4 +99,5 @@ module.exports = {
     deleteATrainee
 
 }
+
 
